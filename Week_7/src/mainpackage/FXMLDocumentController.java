c3b9d8111506a836289b02cd4715227da8fc9c85
m7942 <m7942@lut1824.pc.lut.fi>
2016-06-09 14:58:29 +0300

    week 7 alkuP valmis

--- Week_7/src/mainpackage/FXMLDocumentController.java
+++ Week_7/src/mainpackage/FXMLDocumentController.java
@@ -11,26 +11,73 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
 
 /**
  *
  * @author m7942
  */
 public class FXMLDocumentController implements Initializable {
-    
+    @FXML
+    private Button buttonInput;
     @FXML
     private Label label;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Slider sizeSlider;
+    @FXML
+    private TextField inputField;
+    @FXML
+    private CheckBox pickWorld;
+    private boolean checked;
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        System.out.println("Hello world!");
+        if(checked == false){
+        label.setText(inputField.getText());
+        }
     }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clearAction(ActionEvent event) {
+        if(checked == false){
+        label.setText(null);
+        }
+        inputField.clear();
+    }
+
+    @FXML
+    private void sliderChange(MouseEvent event) {
+        if(checked == false){
+        System.out.println("Slideriä muutettu :d");
+        label.setFont(new Font(sizeSlider.getValue()));
+        }
+    }
+
+    @FXML
+    private void yesWorld(ActionEvent event) {
+        checked = pickWorld.selectedProperty().get();
+        if(checked == true){
+            label.setText("HELLO WORLD!!! :DDDDDD");
+        }
+        else{
+            label.setText("TEKSTIÄ");
+        }
+        }
+    }
     
-}
